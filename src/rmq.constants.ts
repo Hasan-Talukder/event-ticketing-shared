0e6f9672e90991e6ddd0d9425514004e77a9e9a9
Hasan Talukder <mahadihasansiam2@gmail.com>
2026-01-16 17:16:30 +0600

    format RMQ_SERVICE and RMQ_QUEUE for improved readability

--- src/rmq.constants.ts
+++ src/rmq.constants.ts
@@ -1,8 +1,12 @@
 /** @format */
 
-export const RMQ_SERVICE = { AUTH: "AUTH_SERVICE" } as const;
+export const RMQ_SERVICE = {
+  AUTH: "AUTH_SERVICE",
+} as const;
 
-export const RMQ_QUEUE = { AUTH: "auth_queue" } as const;
+export const RMQ_QUEUE = {
+  AUTH: "auth_queue",
+} as const;
 
 export const RMQ_PATTERN = {
   REGISTER: { cmd: "auth.register" },
