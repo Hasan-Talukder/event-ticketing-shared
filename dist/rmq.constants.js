77f6f73504755e28b6cc455f606fc85a4f87f57c
Hasan Talukder <mahadihasansiam2@gmail.com>
2026-01-16 17:24:46 +0600

    format RMQ_SERVICE and RMQ_QUEUE for improved readability

--- dist/rmq.constants.js
+++ dist/rmq.constants.js
@@ -2,8 +2,12 @@
 /** @format */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.RMQ_PATTERN = exports.RMQ_QUEUE = exports.RMQ_SERVICE = void 0;
-exports.RMQ_SERVICE = { AUTH: "AUTH_SERVICE" };
-exports.RMQ_QUEUE = { AUTH: "auth_queue" };
+exports.RMQ_SERVICE = {
+    AUTH: "AUTH_SERVICE",
+};
+exports.RMQ_QUEUE = {
+    AUTH: "auth_queue",
+};
 exports.RMQ_PATTERN = {
     REGISTER: { cmd: "auth.register" },
     LOGIN: { cmd: "auth.login" },
